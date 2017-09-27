0c611c5585ce824c17930c68ec041b1a3a6430f4
PhysRex <ThePhysRex@gmail.com>
2017-09-27 12:02:50 -0400

    fixed formatting, updated content

--- script.js
+++ script.js
@@ -14,14 +14,42 @@ function showCode() {
   }
 
   btnElem.classList.toggle("invert");
-
   
-  resumeElem.classList.toggle("shift");
-  
+  resumeElem.classList.toggle("shift");  
 
 }
 
+function getInnerText(el) {
+  var sel, range, innerText = "";
+  if (typeof document.selection != "undefined" && typeof document.body.createTextRange != "undefined") {
+      range = document.body.createTextRange();
+      range.moveToElementText(el);
+      innerText = range.text;
+  } else if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+      sel = window.getSelection();
+      sel.selectAllChildren(el);
+      innerText = "" + sel;
+      sel.removeAllRanges();
+  }
+  return innerText;
+}
 
+function convertHTML() {
+  const htmlElem = document.body;
+  
+  let codeElem = document.getElementById("codeTag");
+  codeElem.innerText = "";
+
+  let htmlText = htmlElem.parentNode.innerHTML;
+  htmlText = htmlText.replace("</", "&lt;&sol;");
+  htmlText = htmlText.replace("<", "&lt;");
+  htmlText = htmlText.replace(">", "&gt;");
+
+  codeElem.innerHTML = htmlText;
+
+}
+
+convertHTML();
 
 // event listener
 document.getElementById("btn-code").addEventListener("click", ()=> {
