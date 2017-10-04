4c9e41259f84b00691373dfb28b0ea68a4b40430
PhysRex <ThePhysRex@gmail.com>
2017-10-04 15:18:02 -0400

    added projects and other content

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 
+
+
 function showCode() {
   const codeElems = document.getElementsByTagName("pre");
   const resumeElem = document.querySelector("#resume");
@@ -19,6 +21,80 @@ function showCode() {
 
 }
 
+var count = 0;
+function showProjects() {  
+  const resumeElem = document.querySelector("#resume");
+  const projectsElem = document.querySelector("#projects");
+  // const codeBtn = document.querySelector("#btn-code");
+  // const homeBtn = document.querySelector("#btn-home");
+
+  setTimeout(()=>{ 
+    resumeElem.classList.toggle("hide");
+    projectsElem.classList.toggle("hide");
+    
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+  }, 500);
+
+  if (count%2!==0) {    
+    projectsElem.classList.toggle("fade-out");
+    setTimeout(()=>{
+      projectsElem.classList.toggle("fade-out");
+    }, 500);
+  } else {
+    resumeElem.classList.toggle("fade-out");
+    setTimeout(()=>{
+      resumeElem.classList.toggle("fade-out");
+    }, 500);
+  }
+  
+    
+  count++;
+}
+
+function showModal(event) {
+  // FIXME: needs to be completed with auto-loading of content, disabled to make link directly to site
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  
+  // When the user clicks on the button, open the modal 
+  modal.style.display = "block";
+  // modal.classList.toggle("fade-in");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      setTimeout(()=>{modal.style.display = "none";modal.classList.toggle("fade-out");},700);
+      modal.classList.toggle("fade-out");
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target === modal) {
+        setTimeout(()=>{modal.style.display = "none";modal.classList.toggle("fade-out");},700);
+        modal.classList.toggle("fade-out");
+      }
+  }
+
+
+
+
+
+  // replace modal data with correct info
+  // console.log(event.target.tagName);
+  const elem = event.target;
+  const tag = event.target.tagName;
+  var data, name;
+  if (tag==="BUTTON") {
+    data = elem.dataset.target;
+    name = elem.id;
+    // console.log(data);
+  }
+
+}
+
 function getInnerText(el) {
   var sel, range, innerText = "";
   if (typeof document.selection != "undefined" && typeof document.body.createTextRange != "undefined") {
@@ -35,6 +111,7 @@ function getInnerText(el) {
 }
 
 function convertHTML() {
+  // Converting live html for "code" tag on resume
   const htmlElem = document.body;
   
   let codeElem = document.getElementById("codeTag");
@@ -51,7 +128,24 @@ function convertHTML() {
 
 convertHTML();
 
-// event listener
-document.getElementById("btn-code").addEventListener("click", ()=> {
+// event listeners
+document.getElementById("btn-code").addEventListener("click", () => {
   showCode();
 });
+
+document.getElementById("link-portfolio").addEventListener("click", () => {
+  showProjects();
+});
+
+document.getElementById("btn-home").addEventListener("click", () => {
+  showProjects();
+});
+
+// Event listener for modal
+// const siteElems = document.getElementsByClassName("site");
+// for (var elem of siteElems) {
+//   elem.addEventListener("click", (event) => {
+//     showModal(event);
+//   });
+// }
+
